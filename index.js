41337e0e91753bc9251929f90795aee55aa54de0
sureshasoori <sureshasoori60@gmail.com>
2021-11-14 21:47:58 +0530

    nov14

---
+++ index.js
@@ -0,0 +1,20 @@
+function fst(){
+    let a = prompt("enter(YES or NO)", "YES or NO");
+    let b = document.getElementById('fst');
+    b.value = a;
+
+}
+
+function fsst(){
+    let c = prompt("enter(YES or NO)", "YES or NO");
+    let d = document.getElementById('sst');
+    d.value = c;
+
+}
+
+function ftst(){
+    let e = prompt("enter(YES or NO)", "YES or NO");
+    let f = document.getElementById('tst');
+    f.value = e;
+
+}
